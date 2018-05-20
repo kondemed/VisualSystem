8601325770ef07e5796edbccf1c4ee01d96b8e1b
kondemed <knightdsk@outlook.com>
2018-05-20 16:49:27 -0500

    Updated staging functions.

--- Circuitry/Circuitry.ino
+++ Circuitry/Circuitry.ino
@@ -19,7 +19,7 @@ std::<array> horizontal[GANGLIA-1];
 //Functions to build the necessary cells
 //Create the rods
 void stageRods(int N){
-  for (int i;i<N;i++){
+  for (int i=0;i<N;i++){
     rods[i].initialize(Ai);
   }
 }
@@ -31,25 +31,31 @@ void stageHorizontalRow2(){
   
 }
 
-void stageGanglia(int N, rods cell, horizontal surround){
-  for (int i;i<N;i++){
-    int R = 8 + i;
-    ganglion gangliai.addCenter(rodsR);
-    gangliai.addHorizontal(surround);
+void stageGanglia(int N, rods cell, int M, horizontal surround){
+  for (int i=0;i<N;i++){
+    M += i;
+    ganglia[i].addCenter(rods[M]);
+    ganglia[i].addHorizontal(surround[i]);
   }
 }
 
 stageRods(RODS);
-horizontal horizon0 = horizontal.addRecpt(A0);
+/* One example receptive field
+horizontal horizon0;
+horizon0.addRecpt(A0);
 horizon0.addRecpt(A1);
 horizon0.addRecpt(A2);
+horizon0.addRecpt(A5);
 horizon0.addRecpt(A7);
-horizon0.addRecpt(A9);
-horizon0.addRecpt(A11);
 horizon0.addRecpt(A12);
 horizon0.addRecpt(A13);
+ganglion0.addCenter(A6);
+*/
 
-stageGanglia(ganglion0, A8, horizon0);
+stageGanglia(4, ganglia[], rods[], 6, horizontal[]);
+stageGanglia(5, ganglia[], rods[], 12, horizontal[]);
+stageGanglia(4, ganglia[], rods[], 19, horizontal[]);
+
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
